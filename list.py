9ac5d1366747a0ea56bfe37a101729676e822906
Sharmila <sharmilav19@gmail.com>
2026-02-19 06:15:01 +0530

    version3

--- list.py
+++ list.py
@@ -1,11 +1,8 @@
 numbers = list(map(int, input("Enter numbers: ").split()))
-
 unique = []
-
 for num in numbers:
     if num not in unique:
         unique.append(num)
-
 print("List without duplicates:", unique)
 #list
 ##update
